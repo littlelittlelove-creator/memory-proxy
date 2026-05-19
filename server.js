825a8af9641d961680526d4400124334678f617f
littlelittlelove-creator <littlelittlelove@gmail.com>
2026-05-19 15:23:08 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/embed', async (req, res) => {
+  const { text, apiKey } = req.body;
+  try {
+    const response = await axios.post('https://api.siliconflow.cn/v1/embeddings', {
+      model: 'BAAI/bge-large-zh-v1.5',
+      input: text,
+      encoding_format: 'float'
+    }, {
+      headers: { 'Authorization': 'Bearer ' + apiKey, 'Content-Type': 'application/json' }
+    });
+    res.json({ embedding: response.data.data[0].embedding });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(3333, () => console.log('ready on http://localhost:3333'));
